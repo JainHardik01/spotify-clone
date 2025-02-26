673aca0dda781225931f898b9300fc7e0a9a9c0e
Hardik Jain <127095993+JainHardik01@users.noreply.github.com>
2025-02-26 12:35:00 +0530

    Delete .js

--- .js
+++
@@ -1,4 +0,0 @@
-var encodedString = "Agam - Shiv Panchakshara Stotram _ Sai Bhavana _ Lyrical _ %E0%A4%B6%E0%A4%BF%E0%A4%B5 %E0%A4%AA%E0%A4%82%E0%A4%9A%E0%A4%BE%E0%A4%95%E0%A5%8D%E0%A4%B7%E0%A4%B0 %E0%A4%B8%E0%A5%8D%E0%A4%A4%E0%A5%8B%E0%A4%A4%E0%A5%8D%E0%A4%B0%E0%A4%AE%E0%A5%8D _ Mantra _ Shiv.mp3"
-
-var decodedString = decodeURIComponent(encodedString);
-console.log(decodedString);
